21d2abfb9e7e91c67e63bb2b92dddc972c65a02d
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-06-02 11:33:02 +0430

    finish download part

--- event.js
+++ event.js
@@ -1,7 +1,13 @@
-setInterval(function(){
-var myActive=document.getElementsByTagName("li").className
-if(myActive=="active")
-{
-    myActive.style.width="60px"
-}
-},10000)
+// setInterval(function(){
+// var myActive=document.getElementsByTagName("li").className
+// if(myActive=="active")
+// {
+//     myActive.style.width="60px"
+// }
+// },10000)
+$(".partsLogo").on("click",function(){
+    $(".partsLogo").removeClass("partsLogo-active");
+    $(".partsLogo").children().removeClass("partsLogo-img-active")
+    $(this).addClass("partsLogo-active");
+    $(this).children().addClass("partsLogo-img-active")
+})
